22db6c9f7ea5b0011df1f6cb4c9a95b3e743c8b1
coder-day <nykim882@gmail.com>
2021-05-02 00:05:29 +0900

    서블릿 UTF-8

--- src/main/java/com/example/exam/exam1/UsrHomeMain.java
+++ src/main/java/com/example/exam/exam1/UsrHomeMain.java
@@ -7,32 +7,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloServlet
- */
-@WebServlet("/HelloServlet")
-public class HelloServlet extends HttpServlet {
+
+@WebServlet("/usr/home/main")
+public class UsrHomeMain extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HelloServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// GET 파라미터 name 설정 테스트
+		
+		String name = request.getParameter("name");
+		
+		// 들어오는 파라미터 UTF-8로 인코딩
+		request.setCharacterEncoding("UTF-8");
+		
+		//응답 캐릭터 셋 및 type UTF-8 설정 하여 한글 깨짐 방지
+		// 서블릿이 HTML 파일을 만들때 UTF-8로 쓰기
+		response.setCharacterEncoding("UTF-8");
+		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+		response.setContentType("text/html; charset=UTF-8");
+		
+		response.getWriter().append(name);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
